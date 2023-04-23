674d58883a3b46e5741ef8d1986c55115b6bed37
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2023-04-23 14:49:13 -0400

    player movement is improved

--- Player/player.gd
+++ Player/player.gd
@@ -7,6 +7,10 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+var animation_locked: bool = false
+var direction : Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,15 +18,30 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_vector("left", "right", "down", "up")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	updateAnimation()
+	updateMovingDirection()
+func updateAnimation():
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+			
+
+func updateMovingDirection():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	if direction.x < 0:
+		animated_sprite.flip_h = true;
