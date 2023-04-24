32cd38f094f83f7ef41bfaa22c74bd6fe101f5cd
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2023-04-24 13:24:09 -0400

    player now bounces off of lava tiles

--- Player/player.gd
+++ Player/player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
+const LAVA_LAYER = 1 << (2 - 1)    # collision layer 2
+#const ITEM_BLOCK_LAYER = 1 << (6 - 1)  # collision layer 6
+var layer_of_collision = null
 
 @export var speed: float = 120.0
 @export var jump_velocity: float = -200.0
-
+var health = 3
+var player_alive = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var collider: CollisionShape2D = $CollisionShape2D
+# access the animated sprite
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 var animation_locked: bool = false
@@ -29,7 +35,19 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
-	move_and_slide()
+	var collision = move_and_slide()
+	if collision:
+		var collider = get_last_slide_collision().get_collider()
+		if collider is TileMap:
+			var tile_rid = get_last_slide_collision().get_collider_rid()
+			layer_of_collision = PhysicsServer2D.body_get_collision_layer(tile_rid)
+	
+	if layer_of_collision == LAVA_LAYER:
+		print_debug("touched laba")
+		velocity.y = jump_velocity * 1.2
+		layer_of_collision = null
+
+	#move_and_slide()
 	updateAnimation()
 	updateMovingDirection()
 func updateAnimation():
@@ -38,10 +56,11 @@ func updateAnimation():
 			animated_sprite.play("run")
 		else:
 			animated_sprite.play("idle")
-			
 
 func updateMovingDirection():
 	if direction.x > 0:
 		animated_sprite.flip_h = false
 	if direction.x < 0:
 		animated_sprite.flip_h = true;
+
+
